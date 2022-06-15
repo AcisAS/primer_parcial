805ec67ea2ad68da354767aeee0adbe12ed817bb
Acis Avendano Sanchez <acis.avendano@uabc.edu.mx>
2022-06-15 16:49:53 -0700

    Se corrigio ruta de de la ruta principal '/' y se redirecciona a la ruta de profesores

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('principal');
+    return redirect()->route('consulta_general_profesores');
 });
 
 Route::prefix('profesores')->group(function () {
